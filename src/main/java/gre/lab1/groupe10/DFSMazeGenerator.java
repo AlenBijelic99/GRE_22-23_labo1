d3682627cefa54e0b4bab8a2b81bad1f307f642f
Alen Bijelic <alen.bijelic@heig-vd.ch>
2023-03-23 16:00:55 +0100

    Add comments

--- src/main/java/gre/lab1/groupe10/DFSMazeGenerator.java
+++ src/main/java/gre/lab1/groupe10/DFSMazeGenerator.java
@@ -12,64 +12,64 @@ import java.util.*;
  * Générateur de labyrinthe
  */
 public final class DFSMazeGenerator implements MazeGenerator {
-    /**
-     * Algoritme DFS permettant de génèrer un labyrinthe en supprimant les murs entre deux sommets après avoir visité tous les sommets voisins
-     *
-     * @param builder Un builder à qui déléguer les modifications de la structure de données.
-     * @param from    Sommet de départ, si l'algorithme utilisé en nécessite un.
-     */
-    @Override
-    public void generate(MazeBuilder builder, int from) {
-        // Récupération du graphe et du label permettant d'indiquer à l'interface l'état de la progression
-        Graph graph = builder.topology();
-        VertexLabelling<Progression> label = builder.progressions();
+  /**
+   * Algoritme DFS permettant de génèrer un labyrinthe
+   *
+   * @param builder Un builder à qui déléguer les modifications de la structure de données.
+   * @param from    Sommet de départ, si l'algorithme utilisé en nécessite un.
+   */
+  @Override
+  public void generate(MazeBuilder builder, int from) {
+    // Récupération du graphe et du label permettant d'indiquer à l'interface l'état de la progression
+    Graph graph = builder.topology();
+    VertexLabelling<Progression> label = builder.progressions();
 
-        // Défini si un sommet est visité
-        boolean[] visited = new boolean[graph.nbVertices()];
-        Arrays.fill(visited, false);
+    // Défini si un sommet est visité
+    boolean[] visited = new boolean[graph.nbVertices()];
+    Arrays.fill(visited, false);
 
-        // Pile LIFO permettant de stocker les sommets à visiter
-        Stack<Integer> stack = new Stack<>();
+    // Pile LIFO permettant de stocker les sommets à visiter
+    Stack<Integer> stack = new Stack<>();
 
-        // Commence avec le sommet de départ
-        int lastVisited = from;
-        stack.push(from);
-        visited[from] = true;
-        label.setLabel(from, Progression.PROCESSING);
+    // Commence avec le sommet de départ
+    int lastVisited = from;
+    stack.push(from);
+    visited[from] = true;
+    label.setLabel(from, Progression.PROCESSING);
 
-        // Traiter les sommets restants dans la pile
-        while (!stack.isEmpty()) {
-            // Récupération du dernier élément ajouté à la pile
-            int current = stack.peek();
-            // Récupération des voisins du sommet actuel et éffectue mélange pour obtenir la liste des voisins dans un ordre aléatoire
-            List<Integer> neighbors = graph.neighbors(current);
-            Collections.shuffle(neighbors);
+    // Traiter les sommets restants dans la pile
+    while (!stack.isEmpty()) {
+      // Récupération du dernier élément ajouté à la pile
+      int current = stack.peek();
+      // Récupération des voisins du sommet actuel et éffectue mélange pour obtenir la liste des voisins dans un ordre aléatoire
+      List<Integer> neighbors = graph.neighbors(current);
+      Collections.shuffle(neighbors);
 
-            boolean hasNeighborToVisit = false;
-            for (int neighbor : neighbors) {
-                // On traite le premier voisin non visité dans la liste.
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
+      boolean hasNeighborToVisit = false;
+      for (int neighbor : neighbors) {
+        // On traite le premier voisin non visité dans la liste.
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
 
-                    // Supprimer le mur entre le sommet courant et le voisin
-                    //builder.removeWall(current, neighbor);
+          // Supprimer le mur entre le sommet courant et le voisin
+          //builder.removeWall(current, neighbor);
 
-                    // Ajouter le voisin à la pile et le marquer en traitement
-                    stack.push(neighbor);
-                    label.setLabel(neighbor, Progression.PROCESSING);
-                    hasNeighborToVisit = true;
-                    break;
-                }
-            }
-            // Si tous les voisin d'un sommet ont été visités, on peut le marquer comme traité
-            if (!hasNeighborToVisit) {
-                label.setLabel(current, Progression.PROCESSED);
-                lastVisited = stack.pop();
-                // Si ce n'est pas le dernier sommet de la pile on enlève le mur avec l'élément précédemment visité
-                if (!stack.empty()) {
-                    builder.removeWall(stack.peek(), lastVisited);
-                }
-            }
+          // Ajouter le voisin à la pile et le marquer en traitement
+          stack.push(neighbor);
+          label.setLabel(neighbor, Progression.PROCESSING);
+          hasNeighborToVisit = true;
+          break;
         }
+      }
+      // Si tous les voisin d'un sommet ont été visités, on peut le marquer comme traité
+      if (!hasNeighborToVisit) {
+        lastVisited = stack.pop();
+        // Si ce n'est pas le dernier sommet de la pile on enlève le mur avec l'élément précédemment visité
+        if (!stack.empty()) {
+          builder.removeWall(stack.peek(), lastVisited);
+        }
+        label.setLabel(current, Progression.PROCESSED);
+      }
     }
+  }
 }
--- src/main/java/gre/lab1/groupe10/GridGraph.java
+++ src/main/java/gre/lab1/groupe10/GridGraph.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-// TODO: javadoc
+/**
+ * Graphe représentant une grille
+ */
 public final class GridGraph implements GridGraph2D {
   /** Largeur de la grille. */
   private final int width;
@@ -14,6 +16,7 @@ public final class GridGraph implements GridGraph2D {
   /** Hauteur de la grille. */
   private final int height;
 
+  /** List d'adjacence **/
   private final ArrayList<LinkedList<Integer>> adjVerticesList;
 
   /**
@@ -37,12 +40,18 @@ public final class GridGraph implements GridGraph2D {
     this.width = width;
     this.height = height;
 
+    // Initialisation de la liste d'adjacence
     adjVerticesList = new ArrayList<>(width * height);
     for (int i = 0; i < width * height; ++i) {
       adjVerticesList.add(new LinkedList<>());
     }
   }
 
+  /**
+   * Retourne les voisins d'un sommet
+   * @param v Le sommet dont on veut les voisins
+   * @return Liste d'entier représentant les voisins d'un sommet
+   */
   @Override
   public List<Integer> neighbors(int v) {
     if (v < 0 || v >= height * width) {
@@ -56,30 +65,38 @@ public final class GridGraph implements GridGraph2D {
     return neightborsValues;
   }
 
-  private void isInBound(int u, int v){
+  /**
+   * Vérification que deux sommets sont dans le graphe
+   * @param u Premier sommet à vérifier
+   * @param v Deuxième sommet à vérifier
+   */
+  private void areInBound(int u, int v){
     if (!vertexExists(u) || !vertexExists(v)) {
       throw new IndexOutOfBoundsException("u or v doesn't exist.");
     }
   }
 
+  /**
+   * Vérifie que deux sommets son adjacent
+   * @param u Premier sommet
+   * @param v Deuxième sommet.
+   * @return Si le premier sommet est adjacent avec le deuxième et vice-versa
+   */
   @Override
   public boolean areAdjacent(int u, int v) {
-    isInBound(u, v);
+    areInBound(u, v);
 
-    // Adjacent if v is next, above or below to u.
-    //return (u == v - 1 || u == v + 1 || u == v - width || u == v + width);
     return adjVerticesList.get(u).contains(v) && adjVerticesList.get(v).contains(u);
   }
 
+  /**
+   * Lie deux sommets par une arête
+   * @param u Une extrémité de l'arête.
+   * @param v L'autre extrémité de l'arête.
+   */
   @Override
   public void addEdge(int u, int v) {
-    isInBound(u, v);
-
-    /*if (!areAdjacent(u, v)){
-      throw new IllegalArgumentException("u and v aren't adjacent.");
-    }*/
-
-    // TODO: Actuellement ajout simplement la valeur dans la linked list. Il faudra peut être garantir qu'il n'y a qu'une seule valeur possible ou que les valeurs sont dans un ordre croissant.
+    areInBound(u, v);
 
     if (!adjVerticesList.get(u).contains(v)) {
       adjVerticesList.get(u).add(v);
@@ -89,9 +106,14 @@ public final class GridGraph implements GridGraph2D {
     }
   }
 
+  /**
+   * Dissocie deux sommets d'une arête
+   * @param u Une extrémité de l'arête.
+   * @param v L'autre extrémité de l'arête.
+   */
   @Override
   public void removeEdge(int u, int v) {
-    isInBound(u, v);
+    areInBound(u, v);
 
     if (!areAdjacent(u, v)){
       throw new IllegalArgumentException("u and v aren't adjacent.");
@@ -101,21 +123,38 @@ public final class GridGraph implements GridGraph2D {
     adjVerticesList.get(v).removeFirstOccurrence(u);
   }
 
+  /**
+   * Retourne le nombre de sommets
+   * @return Le nombre de sommets
+   */
   @Override
   public int nbVertices() {
     return width * height;
   }
 
+  /**
+   * Indique si un sommet existe dans le graphe
+   * @param v Un sommet.
+   * @return Si un sommet existe dans le graphe
+   */
   @Override
   public boolean vertexExists(int v) {
     return (v >= 0 && v < height * width);
   }
 
+  /**
+   * La largeur de la grille
+   * @return Largeur de la grille
+   */
   @Override
   public int width() {
     return width;
   }
 
+  /**
+   * La hauteur de la grille
+   * @return Hauteur de la grille
+   */
   @Override
   public int height() {
     return height;
