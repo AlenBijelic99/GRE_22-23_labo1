938745a73d064c69dcbf9da57f959937b4d0edbd
Alen Bijelic <alen.bijelic@heig-vd.ch>
2023-03-23 12:26:22 +0100

    Refactor

--- src/main/java/gre/lab1/groupe10/DFSMazeGenerator.java
+++ src/main/java/gre/lab1/groupe10/DFSMazeGenerator.java
@@ -8,47 +8,66 @@ import gre.lab1.gui.Progression;
 
 import java.util.*;
 
-// TODO: javadoc
+/**
+ * Générateur de labyrinthe
+ */
 public final class DFSMazeGenerator implements MazeGenerator {
-  @Override
-  public void generate(MazeBuilder builder, int from) {
-    // TODO: A implémenter
-    //  NOTES D'IMPLÉMENTATION :
-    //  Afin d'obtenir l'affichage adéquat, indiquer la progression (en tant que label du sommet traité) :
-    //  - PROCESSING, en pré-traitement;
-    //  - PROCESSED, en post-traitement.
-    //  Le labyrinthe n'a que des murs au début de la construction, il faut donc créer les passages en
-    //  supprimant des murs.
+    /**
+     * Algoritme DFS permettant de génèrer un labyrinthe en supprimant les murs entre deux sommets après avoir visité tous les sommets voisins
+     *
+     * @param builder Un builder à qui déléguer les modifications de la structure de données.
+     * @param from    Sommet de départ, si l'algorithme utilisé en nécessite un.
+     */
+    @Override
+    public void generate(MazeBuilder builder, int from) {
+        // Récupération du graphe et du label permettant d'indiquer à l'interface l'état de la progression
+        Graph graph = builder.topology();
+        VertexLabelling<Progression> label = builder.progressions();
 
-    Graph graph = builder.topology();
+        // Défini si un sommet est visité
+        boolean[] visited = new boolean[graph.nbVertices()];
+        Arrays.fill(visited, false);
 
-    // Marquer tous les sommets comme non visités
-    boolean[] visited = new boolean[graph.nbVertices()];
-    Arrays.fill(visited, false);
+        // Pile LIFO permettant de stocker les sommets à visiter
+        Stack<Integer> stack = new Stack<>();
 
-    // Initialiser la pile de parcours
-    Stack<Integer> stack = new Stack<>();
+        // Commence avec le sommet de départ
+        int lastVisited = from;
+        stack.push(from);
+        visited[from] = true;
+        label.setLabel(from, Progression.PROCESSING);
 
-    // Traiter le sommet initial
-    stack.push(from);
+        // Traiter les sommets restants dans la pile
+        while (!stack.isEmpty()) {
+            // Récupération du dernier élément ajouté à la pile
+            int current = stack.peek();
+            // Récupération des voisins du sommet actuel et éffectue mélange pour obtenir la liste des voisins dans un ordre aléatoire
+            List<Integer> neighbors = graph.neighbors(current);
+            Collections.shuffle(neighbors);
 
-    VertexLabelling<Progression> label = builder.progressions();
-    int lastVisited = from;
-    while (!stack.isEmpty()) {
-      int current = stack.pop();
-      label.setLabel(current, Progression.PROCESSING);
-      List<Integer> neighbors = graph.neighbors(current);
-      Collections.shuffle(neighbors);
-      for (int neighbor : neighbors) {
-        if (!visited[neighbor]) {
-          stack.push(neighbor);
-          visited[neighbor] = true;
-        } else if (graph.areAdjacent(current, neighbor) && neighbor != lastVisited) {
-          builder.removeWall(current, neighbor);
+            boolean hasNeighborToVisit = false;
+            for (int neighbor : neighbors) {
+              // On traite le premier voisin non visité dans la liste.
+              if (!visited[neighbor]) {
+                visited[neighbor] = true;
+
+                // Supprimer le mur entre le sommet courant et le voisin
+                //builder.removeWall(current, neighbor);
+
+                // Ajouter le voisin à la pile et le marquer en traitement
+                stack.push(neighbor);
+                label.setLabel(neighbor, Progression.PROCESSING);
+                hasNeighborToVisit = true;
+                break;
+              }
+            }
+            if (!hasNeighborToVisit) {
+                stack.pop();
+                if (graph.areAdjacent(current, lastVisited))
+                  builder.removeWall(current, lastVisited);
+                label.setLabel(current, Progression.PROCESSED);
+            }
+          lastVisited = current;
         }
-      }
-      label.setLabel(current, Progression.PROCESSED);
-      lastVisited = current;
     }
-  }
 }
